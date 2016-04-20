ee08470b293e12dc718623cc856963e5c036bf40
Richard Lau <richardlau.rlau@gmail.com>
2016-04-20 09:46:22 -0400

    Created new Account Class and fixed Test 1

---
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+public class Account {
+    private int balance;
+
+    public Account(int initialAmount) {
+        this.balance = initialAmount;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
