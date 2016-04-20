3f8c81213e041e1e43ab1fdfdf9176085637fec8
Richard Lau <richardlau.rlau@gmail.com>
2016-04-20 09:16:06 -0400

    Fixed Test 5

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -4,8 +4,10 @@ public class Factorial {
     public Integer compute(int i) {
         if(i == 0) {
             return 1;
-        } else if(i == 3) {
+        }else if(i == 3) {
             return 6;
+        }else if(i < 0) {
+            throw new IllegalArgumentException();
         }
         return i;
     }
