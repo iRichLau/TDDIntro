4d90c0fac68a52540bf9b6bf556c15b877c40e95
Richard Lau <richardlau.rlau@gmail.com>
2016-04-20 10:08:29 -0400

    Fixed Test 3

--- src/test/java/com/thoughtworks/tddintro/accountbalance/Account.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -16,6 +16,8 @@ public class Account {
     }
 
     public void withdraw(int amount) {
-        this.balance -= amount;
+        if(amount < balance) {
+            this.balance -= amount;
+        }
     }
 }
