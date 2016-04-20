f496e2886b1a8075d477af519197922d8815ca26
Richard Lau <richardlau.rlau@gmail.com>
2016-04-20 09:50:57 -0400

    Fixed Test 2

--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -14,11 +14,13 @@ public class AccountTests {
         assertThat(account.getBalance(), is(150));
     }
 
-//    @Test
-//    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-//
-//    }
-//
+    @Test
+    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
+        Account account = new Account(100);
+        account.withdraw(50);
+        assertThat(account.getBalance(), is(50));
+    }
+
 //    @Test
 //    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
 //
